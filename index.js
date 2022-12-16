14f727ee3fb71beeb287c3935d753fca9888bade
petersamson-kingori <peters.kingori@student.moringaschool.com>
2022-12-16 14:42:44 +0300

    message

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "peter"
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    window.bestCustomer = "not bob";
+  }
+
+  function overwriteBestCustomer() {
+    window.bestCustomer = "maybe bob";
+  }
+
+ 
+
+  function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Mike";
+  }
+
+
+console.log(customerName);
